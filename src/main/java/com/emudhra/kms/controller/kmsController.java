8c776823f81a439ba79d53d06a9127bb54034862
ratnadipbharde <ratnadipbharde@gmail.com>
2024-04-22 00:35:58 +0530

    project initialise

---
+++ src/main/java/com/emudhra/kms/controller/kmsController.java
@@ -0,0 +1,9 @@
+package com.emudhra.kms.controller;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class kmsController {
+}
